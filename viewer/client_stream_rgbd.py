e989a670b1375db62d3da35da86e7672c53ce1e4
sguptasarma <sguptasarma@gmail.com>
2024-07-23 17:38:34 -0700

    Add depth anything

---
+++ viewer/client_stream_rgbd.py
@@ -0,0 +1,151 @@
+#------------------------------------------------------------------------------
+# This script receives video from the HoloLens front RGB camera and plays it.
+# The camera supports various resolutions and framerates. See
+# https://github.com/jdibenes/hl2ss/blob/main/etc/pv_configurations.txt
+# for a list of supported formats. The default configuration is 1080p 30 FPS. 
+# The stream supports three operating modes: 0) video, 1) video + camera pose, 
+# 2) query calibration (single transfer).
+# Press esc to stop.
+#------------------------------------------------------------------------------
+
+from pynput import keyboard
+
+import cv2
+import hl2ss_imshow
+import hl2ss
+import hl2ss_lnm
+
+import torch
+import matplotlib.pyplot as plt
+from metric_depth.depth_anything_v2.dpt import DepthAnythingV2
+
+
+# load the depth anything model
+DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+
+model_configs = {
+    'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+    'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+    'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+    'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+}
+
+encoder = 'vitb' # or 'vits', 'vitb'
+dataset = 'hypersim' # 'hypersim' for indoor model, 'vkitti' for outdoor model
+max_depth = 20 # 20 for indoor model, 80 for outdoor model
+
+model = DepthAnythingV2(**{**model_configs[encoder], 'max_depth': max_depth})
+model.load_state_dict(torch.load(f'/home/shivani/Depth-Anything-V2/checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth', map_location='cpu'))
+
+model = model.to(DEVICE).eval()
+
+# Settings --------------------------------------------------------------------
+
+# HoloLens address
+host = "192.169.1.41"
+
+# Operating mode
+# 0: video
+# 1: video + camera pose
+# 2: query calibration (single transfer)
+mode = hl2ss.StreamMode.MODE_1
+
+# Enable Mixed Reality Capture (Holograms)
+enable_mrc = False
+
+# Enable Shared Capture
+# If another program is already using the PV camera, you can still stream it by
+# enabling shared mode, however you cannot change the resolution and framerate
+shared = False
+
+# Camera parameters
+# Ignored in shared mode
+width     = 1920
+height    = 1080
+framerate = 30
+
+# Framerate denominator (must be > 0)
+# Effective FPS is framerate / divisor
+divisor = 5
+
+# Video encoding profile
+profile = hl2ss.VideoProfile.H265_MAIN
+
+# Decoded format
+# Options include:
+# 'bgr24'
+# 'rgb24'
+# 'bgra'
+# 'rgba'
+# 'gray8'
+decoded_format = 'bgr24' # for openCV
+
+#------------------------------------------------------------------------------
+
+hl2ss_lnm.start_subsystem_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO, enable_mrc=enable_mrc, shared=shared)
+
+if (mode == hl2ss.StreamMode.MODE_2):
+    data = hl2ss_lnm.download_calibration_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO, width, height, framerate)
+    print('Calibration')
+    print(f'Focal length: {data.focal_length}')
+    print(f'Principal point: {data.principal_point}')
+    print(f'Radial distortion: {data.radial_distortion}')
+    print(f'Tangential distortion: {data.tangential_distortion}')
+    print('Projection')
+    print(data.projection)
+    print('Intrinsics')
+    print(data.intrinsics)
+    print('RigNode Extrinsics')
+    print(data.extrinsics)
+else:
+    enable = True
+
+    def on_press(key):
+        global enable
+        enable = key != keyboard.Key.esc
+        return enable
+
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    client = hl2ss_lnm.rx_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO, mode=mode, width=width, height=height, framerate=framerate, divisor=divisor, profile=profile, decoded_format=decoded_format)
+    client.open()
+
+    while (enable):
+        data = client.get_next_packet()
+
+        print(f'Pose at time {data.timestamp}')
+        print(data.pose)
+        print(f'Focal length: {data.payload.focal_length}')
+        print(f'Principal point: {data.payload.principal_point}')
+
+        cv2.imshow('Video', data.payload.image)
+        # load data & visulaize result
+        raw_img = data.payload.image
+        depth_output = model.infer_image(raw_img) # HxW raw depth map in numpy
+
+        # Normalize the depth map for display (necessary for proper color mapping)
+        normalized_depth = cv2.normalize(depth_output, None, 0, 255, cv2.NORM_MINMAX)
+        normalized_depth = normalized_depth.astype('uint8')
+
+        # Apply a color map to the normalized depth map
+        colored_depth = cv2.applyColorMap(normalized_depth, cv2.COLORMAP_JET)
+
+        # Display the pseudo-colored depth map
+        cv2.imshow('Processed Video', colored_depth)
+        cv2.waitKey(1)
+
+        # print the metric depth of the center pixel
+        center_pixel = (int(normalized_depth.shape[1] / 2), int(normalized_depth.shape[0] / 2))
+        print(f"Depth at center pixel: {depth_output[center_pixel[1], center_pixel[0]]}")
+        
+
+
+    client.close()
+    listener.join()
+
+hl2ss_lnm.stop_subsystem_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO)
+
+
+
+
