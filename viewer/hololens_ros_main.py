686f01681431fe3e1ba33346c121f18bfbb18097
yuzhongchun17 <zhongchun.yu@outlook.com>
2024-08-01 22:09:55 -0700

    publish hololens data and broadcaster tf to ros

---
+++ viewer/hololens_ros_main.py
@@ -0,0 +1,75 @@
+import cv2
+import hl2ss_imshow
+import hl2ss
+import hl2ss_lnm
+
+import rospy
+from hololens_ros_utils import broadcast_tf, publish_camera_data
+from sensor_msgs.msg import Image, CameraInfo
+from tf2_ros import TransformBroadcaster
+
+# Settings --------------------------------------------------------------------
+
+# HoloLens address
+host = "192.169.1.6"
+
+# Operating mode
+# 0: video
+# 1: video + camera pose
+# 2: query calibration (single transfer)
+mode = hl2ss.StreamMode.MODE_1
+
+enable_mrc = False # Enable Mixed Reality Capture (Holograms)
+shared = False # Enable Shared Capture
+
+# Camera parameters
+width     = 1920
+height    = 1080
+framerate = 30
+
+divisor = 1 # Effective FPS is framerate / divisor
+profile = hl2ss.VideoProfile.H265_MAIN # Video encoding profile
+decoded_format = 'bgr24' # Decoded format
+
+def main():
+    # Initialilze ROS node
+    rospy.init_node('hololens2_data_publisher')
+    # rate = rospy.Rate(10) # set loop rate
+
+    # Declare pub and br
+    image_pub = rospy.Publisher("/camera/image_rect", Image, queue_size=10)
+    info_pub = rospy.Publisher("/camera/camera_info", CameraInfo, queue_size=10)
+    br = TransformBroadcaster()
+
+    # # Set the rates for the image publisher and the TF broadcaster
+    # image_rate = rospy.Rate(120)  
+    # tf_rate = rospy.Rate(30)     
+
+    # Start the client object to get HoloLens2 data
+    hl2ss_lnm.start_subsystem_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO, enable_mrc=enable_mrc, shared=shared)
+    client = hl2ss_lnm.rx_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO, mode=mode, width=width, height=height, framerate=framerate, divisor=divisor, profile=profile, decoded_format=decoded_format)
+    client.open()
+
+    # loop inside this
+    while not rospy.is_shutdown():
+        # Get the next data packet from HoloLens2
+        data = client.get_next_packet()
+
+        # Publish image data + broadcast tf
+        broadcast_tf(data.pose, br=br) # ~10 HZ
+        publish_camera_data(data.payload.image, image_pub=image_pub, info_pub=info_pub) # ~ 120 HZ
+
+        # # Sleep to maintain the loop rate
+        # tf_rate.sleep()
+        # image_rate.sleep()
+
+
+    # after the loop, exit
+    client.close()
+    hl2ss_lnm.stop_subsystem_pv(host, hl2ss.StreamPort.PERSONAL_VIDEO)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
---
+++ viewer/hololens_ros_utils.py
@@ -0,0 +1,77 @@
+# #!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import TransformStamped
+from tf2_ros import TransformBroadcaster
+from tf_conversions import transformations 
+
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+# INITIALIZE NODE
+# rospy.init_node('hololens2_data_publisher', anonymous=True)
+
+# DECLARE 
+# NOTE: Also declare publisher and broadcaster outside the loop
+# image_pub = rospy.Publisher("/camera/image_rect", Image, queue_size=10)
+# info_pub = rospy.Publisher("/camera/camera_info", CameraInfo, queue_size=10)
+# br = TransformBroadcaster()
+
+# ------------------------- publisher for /camera/camera_info and /camera/image_rect (for apriltag_ros) -----------------------------
+def publish_camera_data(image, image_pub, info_pub):
+    bridge = CvBridge()
+
+    # Set a fixed camera info (adjust parameters as needed)
+    camera_info = CameraInfo()
+    camera_info.header.frame_id = "camera"
+    camera_info.height = 1920
+    camera_info.width = 1080
+    camera_info.K = [-1467.6746, 0.0, 946.79413, 0.0, 1472.0300, 509.88123, 0.0, 0.0, 1.0]  # Example intrinsic matrix
+    camera_info.P = [-1467.6746, 0.0, 946.79413, 0.0, 0.0, 1472.03, 509.88123, 0.0, 0.0, 0.0, 1.0, 0.0]
+
+    camera_info.header.stamp = rospy.Time.now()
+    image_msg = bridge.cv2_to_imgmsg(image, "bgr8")
+    image_msg.header = camera_info.header
+    image_pub.publish(image_msg)
+    info_pub.publish(camera_info)
+
+    # rate = rospy.Rate(120)  # 10hz
+    # rate.sleep()
+
+# ------------------------- tf broadcaster -----------------------------
+def broadcast_tf(camera_pose, br):
+    # Create a TransformStamped message for world to camera
+    world_to_camera = TransformStamped()
+    world_to_camera.header.frame_id = "hl_world"
+    world_to_camera.child_frame_id = "camera"
+
+    # Extract translation from the matrix
+    camera_pose = camera_pose.T # transpose because the output from hl2ss data.pose is transposed
+    translation = camera_pose[0:3, 3]
+    world_to_camera.transform.translation.x = translation[0]
+    world_to_camera.transform.translation.y = translation[1]
+    world_to_camera.transform.translation.z = translation[2]
+
+    # Extract rotation matrix and convert to quaternion
+    quaternion = transformations.quaternion_from_matrix(camera_pose)
+    world_to_camera.transform.rotation.x = quaternion[0]
+    world_to_camera.transform.rotation.y = quaternion[1]
+    world_to_camera.transform.rotation.z = quaternion[2]
+    world_to_camera.transform.rotation.w = quaternion[3]
+
+    
+    world_to_camera.header.stamp = rospy.Time.now()
+    br.sendTransform(world_to_camera)
+
+    # rate = rospy.Rate(10)  # 10 Hz
+    # rate.sleep()
+
+# TESTER
+# if __name__ == '__main__':
+#     try:
+#         camera_pose = []
+#         broadcast_transforms(camera_pose)
+#     except rospy.ROSInterruptException:
+#         pass
